084d13a9006f12b27fdcf9d3cc264dcf6d2c8d30
Danny Bishop <dannybwebdev@gmail.com>
2023-08-04 16:59:53 -0400

    set up cms

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,13 @@
+const yaml = require("js-yaml");
+
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
+
+  eleventyConfig.addPassthroughCopy({
+    "./src/admin/config.yml": "./admin/config.yml",
+  });
+
     // Return your Object options:
     return {
       dir: {
